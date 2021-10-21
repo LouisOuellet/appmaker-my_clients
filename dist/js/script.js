e00e4a00853d2df9da558c66ec7a55e60f6f913b
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-21 14:51:22 -0400

    21.10-dev-21

--- dist/js/script.js
+++ dist/js/script.js
@@ -59,9 +59,9 @@ API.Plugins.my_clients = {
 				API.request('my_clients','read',{data:{}},function(result) {
 					var dataset = JSON.parse(result);
 					if(dataset.success != undefined){
-						for(const [key, value] of Object.entries(dataset.output.results)){ API.Helper.set(API.Contents,['data','dom','my_clients',value.name],value); }
+						for(const [key, value] of Object.entries(dataset.output.dom)){ API.Helper.set(API.Contents,['data','dom','my_clients',value.name],value); }
 						for(const [key, value] of Object.entries(dataset.output.raw)){ API.Helper.set(API.Contents,['data','raw','my_clients',value.name],value); }
-						API.Builder.table(card.children('.card-body'), dataset.output.results, {
+						API.Builder.table(card.children('.card-body'), dataset.output.dom, {
 							headers:dataset.output.headers,
 							id:'my_clientsIndex',
 							modal:true,
